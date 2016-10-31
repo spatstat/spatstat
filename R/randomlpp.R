5da1a8d9080146a01a61eceeb34ca2fe29aff165
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2016-10-31 09:35:47 +0800

    Bug fixes for random points on linear network.
       randomlpp.R
       rpoislpp.Rd
       runiflpp.Rd
    	Bug fixes
    	New argument 'drop'
    
       DESCRIPTION
       NEWS
    	updated as version 1.47-0.018

--- R/randomlpp.R
+++ R/randomlpp.R
@@ -3,37 +3,41 @@
 #
 #  Random point pattern generators for a linear network
 #
-#  $Revision: 1.4 $   $Date: 2015/02/17 06:15:44 $
+#  $Revision: 1.5 $   $Date: 2016/10/31 00:49:56 $
 #
 
-rpoislpp <- function(lambda, L, ..., nsim=1) {
+rpoislpp <- function(lambda, L, ..., nsim=1, drop=TRUE) {
   if(missing(L) || is.null(L)) {
     if(!inherits(lambda, c("linim", "linfun")))
       stop("L is missing", call.=FALSE)
     L <- as.linnet(lambda)
   } else verifyclass(L, "linnet")
   result <- vector(mode="list", length=nsim)
-  for(i in 1:nsim) {
-    X <- datagen.rpoisppOnLines(lambda, as.psp(L), ...)
+  S <- as.psp(L)
+  bugout <- (nsim == 1) && drop
+  for(i in seq_len(nsim)) {
+    X <- datagen.rpoisppOnLines(lambda, S, ...)
     Y <- lpp(X, L)
-    if(nsim == 1) return(Y)
+    if(bugout) return(Y)
     result[[i]] <- Y
   }
-  Y <- as.solist(Y)
-  names(Y) <- paste("Simulation", 1:nsim)
-  return(Y)
+  result <- as.solist(result)
+  if(nsim > 0) names(result) <- paste("Simulation", 1:nsim)
+  return(result)
 }
 
-runiflpp <- function(n, L, nsim=1) {
+runiflpp <- function(n, L, nsim=1, drop=TRUE) {
   verifyclass(L, "linnet")
   result <- vector(mode="list", length=nsim)
-  for(i in 1:nsim) {
-    X <- datagen.runifpointOnLines(n, as.psp(L))
+  S <- as.psp(L)
+  bugout <- (nsim == 1) && drop
+  for(i in seq_len(nsim)) {
+    X <- datagen.runifpointOnLines(n, S)
     Y <- lpp(X, L)
-    if(nsim == 1) return(Y)
+    if(bugout) return(Y)
     result[[i]] <- Y
   }
-  Y <- as.solist(Y)
-  names(Y) <- paste("Simulation", 1:nsim)
-  return(Y)
+  result <- as.solist(result)
+  if(nsim > 0) names(result) <- paste("Simulation", 1:nsim)
+  return(result)
 }
