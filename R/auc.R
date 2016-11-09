a62f0c2f29a6a5e9eaab5cb51557e03ab0fbb4de
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2016-11-09 19:40:30 +0800

    More methods for roc and auc
       auc.R
       auc.Rd
       roc.Rd
       spatstat-internal.Rd
    	Methods for auc and roc for kppm, lppm, lpp
    
       DESCRIPTION
       NEWS
    	Nickname changed to 'Model Village'
    	Updated as version 1.47-0.024

--- R/auc.R
+++ R/auc.R
@@ -3,12 +3,24 @@
 ##
 ##  Calculate ROC curve or area under it
 ##
-## $Revision: 1.4 $ $Date: 2015/04/15 09:23:22 $
+## $Revision: 1.5 $ $Date: 2016/11/09 10:57:14 $
 
 roc <- function(X, ...) { UseMethod("roc") }
 
 roc.ppp <- function(X, covariate, ..., high=TRUE) {
-  d <- spatialCDFframe(ppm(X), covariate, ...)
+  nullmodel <- ppm(X)
+  result <- rocData(covariate, nullmodel, ..., high=high)
+  return(result)
+}
+
+roc.lpp <- function(X, covariate, ..., high=TRUE) {
+  nullmodel <- lppm(X)
+  result <- rocData(covariate, nullmodel, ..., high=high)
+  return(result)
+}
+
+rocData <- function(covariate, nullmodel, ..., high=TRUE) {
+  d <- spatialCDFframe(nullmodel, covariate, ...)
   U <- d$values$U
   ec <- if(high) ecdf(1-U) else ecdf(U)
   p <- seq(0,1,length=1024)
@@ -30,7 +42,35 @@ roc.ppm <- function(X, ...) {
   model <- X
   lambda <- predict(model, ...)
   Y <- data.ppm(model)
-  d <- spatialCDFframe(ppm(Y), lambda, ...)
+  nullmodel <- ppm(Y)
+  result <- rocModel(lambda, nullmodel, ...)
+  return(result)
+}
+
+roc.kppm <- function(X, ...) {
+  stopifnot(is.kppm(X))
+  model <- as.ppm(X)
+  lambda <- predict(model, ...)
+  Y <- data.ppm(model)
+  nullmodel <- ppm(Y)
+  result <- rocModel(lambda, nullmodel, ...)
+  return(result)
+}
+
+roc.lppm <- function(X, ...) {
+  stopifnot(is.lppm(X))
+  model <- X
+  lambda <- predict(model, ...)
+  Y <- X$X
+  nullmodel <- lppm(Y)
+  result <- rocModel(lambda, nullmodel, ...)
+  return(result)
+}
+
+rocModel <- function(lambda, nullmodel, ..., high) {
+  if(!missing(high))
+    warning("Argument 'high' is ignored when computing ROC for a fitted model")
+  d<- spatialCDFframe(nullmodel, lambda, ...) 
   U <- d$values$U
   ec <- ecdf(1-U) 
   p <- seq(0,1,length=1024)
@@ -56,6 +96,8 @@ roc.ppm <- function(X, ...) {
   return(result)
 }
 
+#    ......................................................
+
 auc <- function(X, ...) { UseMethod("auc") }
 
 auc.ppp <- function(X, covariate, ..., high=TRUE) {
@@ -65,6 +107,16 @@ auc.ppp <- function(X, covariate, ..., high=TRUE) {
   result <- if(high) EU else (1 - EU) 
   return(result)
 }
+
+auc.lpp <- function(X, covariate, ..., high=TRUE) {
+  d <- spatialCDFframe(lppm(X), covariate, ...)
+  U <- d$values$U
+  EU <- mean(U)
+  result <- if(high) EU else (1 - EU) 
+  return(result)
+}
+
+auc.kppm <- function(X, ...) { auc(as.ppm(X), ...) }
 
 auc.ppm <- function(X, ...) {
   model <- X
@@ -81,6 +133,26 @@ auc.ppm <- function(X, ...) {
     lambda <- as.im(lambda, Window(model))
     X <- data.ppm(model)
     lamX <- lambda[X]
+    aobs <- mean(Fl(lamX))
+    atheo <- mean(lambda[] * Fl(lambda[]))/mean(lambda)
+  }
+  result <- c(aobs, atheo)
+  names(result) <- c("obs", "theo")
+  return(result)
+}
+
+auc.lppm <- function(X, ...) {
+  stopifnot(inherits(X, "lppm"))
+  model <- X
+  if(is.multitype(model)) {
+    # cheat
+    ro <- roc(model, ...)
+    aobs <- with(ro, mean(fobs))
+    atheo <- with(ro, mean(ftheo))
+  } else {
+    lambda <- predict(model, ...)
+    Fl <- ecdf(lambda[])
+    lamX <- lambda[model$X]
     aobs <- mean(Fl(lamX))
     atheo <- mean(lambda[] * Fl(lambda[]))/mean(lambda)
   }
