56ad849160b185f321a6c95a07b634d89d350d2e
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2016-12-02 17:31:04 +0800

    Faster algorithm for density.psp
       segdens.c [New file]
       	C implementation of density.psp exact calculation.
    
       density.psp.R
       density.psp.Rd
    	New argument 'method'
    	Default method is now 'FFT' which is 10-100 times faster.
    
       DESCRIPTION
       NEWS
    	Updated as version 1.47-0.042

--- R/density.psp.R
+++ R/density.psp.R
@@ -2,12 +2,14 @@
 #
 #  density.psp.R
 #
-#  $Revision: 1.6 $    $Date: 2014/08/04 09:49:22 $
+#  $Revision: 1.7 $    $Date: 2016/12/02 08:49:40 $
 #
 #
 
-density.psp <- function(x, sigma, ..., edge=TRUE) {
+density.psp <- function(x, sigma, ..., edge=TRUE,
+                        method=c("FFT", "C", "interpreted")) {
   verifyclass(x, "psp")
+  method <- match.arg(method)
   w <- x$window
   n <- x$n
   if(missing(sigma))
@@ -18,24 +20,52 @@ density.psp <- function(x, sigma, ..., edge=TRUE) {
     return(as.im(0, w))
   #
   ang <- angles.psp(x, directed=TRUE)
-  coz <- cos(ang)
-  zin <- sin(ang)
   xy <- rasterxy.mask(w)
   xx <- xy$x
   yy <- xy$y
-  # compute matrix contribution from each segment 
-  for(i in seq_len(n)) {
-    en <- x$ends[i,]
-    dx <- xx - en$x0
-    dy <- yy - en$y0
-    u1 <- dx * coz[i] + dy * zin[i]
-    u2 <- - dx * zin[i] + dy * coz[i]
-    value <- dnorm(u2, sd=sigma) *
-      (pnorm(u1, sd=sigma) - pnorm(u1-len[i], sd=sigma))
-    totvalue <- if(i == 1) value else (value + totvalue)
-  }
-  dens <- im(totvalue, w$xcol, w$yrow)
-  if(edge) {
+  switch(method,
+         interpreted = {
+           #' compute matrix contribution from each segment 
+           coz <- cos(ang)
+           zin <- sin(ang)
+           for(i in seq_len(n)) {
+             en <- x$ends[i,]
+             dx <- xx - en$x0
+             dy <- yy - en$y0
+             u1 <- dx * coz[i] + dy * zin[i]
+             u2 <- - dx * zin[i] + dy * coz[i]
+             value <- dnorm(u2, sd=sigma) *
+               (pnorm(u1, sd=sigma) - pnorm(u1-len[i], sd=sigma))
+             totvalue <- if(i == 1) value else (value + totvalue)
+           }
+           dens <- im(totvalue, w$xcol, w$yrow)
+         },
+         C = {
+           #' C implementation of the above
+           xs <- x$ends$x0
+           ys <- x$ends$y0
+           xp <- as.numeric(as.vector(xx))
+           yp <- as.numeric(as.vector(yy))
+           np <- length(xp)
+           z <- .C("segdens",
+                   sigma = as.double(sigma),
+                   ns = as.integer(n),
+                   xs = as.double(xs),
+                   ys = as.double(ys),
+                   alps = as.double(ang),
+                   lens = as.double(len),
+                   np = as.integer(np),
+                   xp = as.double(xp), 
+                   yp = as.double(yp),
+                   z = as.double(numeric(np)))
+           dens <- im(z$z, w$xcol, w$yrow)
+         },
+         FFT = {
+           L <- pixellate(x, ...)
+           dens <- blur(L, sigma, normalise=edge, bleed=FALSE)
+         })
+  unitname(dens) <- unitname(x)
+  if(edge && method != "FFT") {
     edg <- second.moment.calc(midpoints.psp(x), sigma, what="edge", ...)
     dens <- eval.im(dens/edg)
   }
