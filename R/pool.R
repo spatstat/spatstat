091df02a95927ef0b832ad5f5dd2d680d993d9f8
Adrian Baddeley <adrian.baddeley@curtin.edu.au>
2017-05-16 12:28:13 +0200

    bug fix

--- R/pool.R
+++ R/pool.R
@@ -41,8 +41,27 @@ pool.fv <- local({
     attributes(sumY) <- attributes(template)
     ## ratio-of-sums
     Ratio <- eval.fv(sumY/sumX, relabel=FALSE)
+    if(variance) {
+      ## variance calculation
+      meanX <- sumX/n
+      meanY <- eval.fv(sumY/n, relabel=FALSE)
+      sumY2 <- Reduce(Add, lapply(Y, Square))
+      varX   <- (sumX2 - n * meanX^2)/(n-1)
+      varY   <- eval.fv((sumY2 - n * meanY^2)/(n-1), relabel=FALSE)
+      sumXY <- Reduce(Add, XY)
+      covXY <- eval.fv((sumXY - n * meanX * meanY)/(n-1), relabel=FALSE)
+      ## variance by delta method
+      relvar <- eval.fv(pmax.int(0, varY/meanY^2 + varX/meanX^2
+                                 - 2 * covXY/(meanX * meanY)),
+                        relabel=FALSE)
+      Variance <- eval.fv(Ratio^2 * relvar/n,
+                          relabel=FALSE)
+      ## two sigma CI
+      hiCI <- eval.fv(Ratio + 2 * sqrt(Variance), relabel=FALSE)
+      loCI <- eval.fv(Ratio - 2 * sqrt(Variance), relabel=FALSE)
+    }
+    ## tweak labels of main estimate
     attributes(Ratio) <- attributes(template)
-    ## tweak labels
     if(relabel)
       Ratio <- prefixfv(Ratio,
                         tagprefix="pool",
@@ -50,24 +69,7 @@ pool.fv <- local({
                         lablprefix="")
     if(!variance)
       return(Ratio)
-    ## variance calculation
-    meanX <- sumX/n
-    meanY <- eval.fv(sumY/n, relabel=FALSE)
-    sumY2 <- Reduce(Add, lapply(Y, Square))
-    varX   <- (sumX2 - n * meanX^2)/(n-1)
-    varY   <- eval.fv((sumY2 - n * meanY^2)/(n-1), relabel=FALSE)
-    sumXY <- Reduce(Add, XY)
-    covXY <- eval.fv((sumXY - n * meanX * meanY)/(n-1), relabel=FALSE)
-    ## variance by delta method
-    relvar <- eval.fv(pmax.int(0, varY/meanY^2 + varX/meanX^2
-                               - 2 * covXY/(meanX * meanY)),
-                      relabel=FALSE)
-    Variance <- eval.fv(Ratio^2 * relvar/n,
-                        relabel=FALSE)
-    ## two sigma CI
-    hiCI <- eval.fv(Ratio + 2 * sqrt(Variance), relabel=FALSE)
-    loCI <- eval.fv(Ratio - 2 * sqrt(Variance), relabel=FALSE)
-    ## relabel
+    ## tweak labels of variance terms
     attributes(Variance) <- attributes(template)
     Variance <- prefixfv(Variance,
                          tagprefix="var",
@@ -82,7 +84,7 @@ pool.fv <- local({
                      tagprefix="lo",
                      descprefix="lower limit of two-sigma CI based on ",
                      lablprefix="bold(lo)~")
-    ##
+    ## glue together
     result <- Reduce(bind.fv, list(Ratio, Variance, hiCI, loCI))
     ## don't plot variances, by default
     fvnames(result, ".") <- setdiff(fvnames(result, "."),
