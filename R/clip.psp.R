c1532b1346c30698c1d8d7c1a6208f6512db3490
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2018-01-23 12:02:39 +0800

    Accelerated clippoly.psp, documented test.crossing.psp

--- R/clip.psp.R
+++ R/clip.psp.R
@@ -1,7 +1,7 @@
 #
 # clip.psp.R
 #
-#    $Revision: 1.22 $   $Date: 2017/11/15 02:06:14 $
+#    $Revision: 1.23 $   $Date: 2018/01/23 02:40:14 $
 #
 #
  
@@ -40,33 +40,95 @@ clip.psp <- function(x, window, check=TRUE, fragments=TRUE) {
 #
 #  clipping to a rectangle
 #
-cliprect.psp <- function(x, window, fragments=TRUE) {
-  verifyclass(x, "psp")
-  verifyclass(window, "owin")
-  ends <- x$ends
-  marx <- marks(x, dfok=TRUE)
-  # find segments which are entirely inside the window
-  # (by convexity)
-  in0 <- inside.owin(ends$x0, ends$y0, window)
-  in1 <- inside.owin(ends$x1, ends$y1, window)
-  ok <- in0 & in1
-  # if all segments are inside, return them
-  if(all(ok))
-    return(as.psp(ends, window=window, marks=marx, check=FALSE))
-  # otherwise, store those segments which are inside the window
-  ends.inside <- ends[ok, , drop=FALSE]
-  marks.inside <- marx %msub% ok
-  x.inside <- as.psp(ends.inside, window=window, marks=marks.inside, check=FALSE)
-  if(!fragments)
-    return(x.inside)
-  # now consider the rest
-  ends <- ends[!ok, , drop=FALSE]
-  in0 <- in0[!ok] 
-  in1 <- in1[!ok]
-  marx <- marx %msub% (!ok)
-  # first clip segments to the range x \in [xmin, xmax]
-  # use parametric coordinates
+cliprect.psp <- local({
+  
+  cliprect.psp <- function(x, window, fragments=TRUE) {
+    verifyclass(x, "psp")
+    verifyclass(window, "owin")
+    ends <- x$ends
+    marx <- marks(x, dfok=TRUE)
+    #' find segments which are entirely inside the window
+    #' (by convexity)
+    in0 <- inside.owin(ends$x0, ends$y0, window)
+    in1 <- inside.owin(ends$x1, ends$y1, window)
+    ok <- in0 & in1
+    #' if all segments are inside, return them
+    if(all(ok))
+      return(as.psp(ends, window=window, marks=marx, check=FALSE))
+    #' otherwise, store those segments which are inside the window
+    ends.inside <- ends[ok, , drop=FALSE]
+    marks.inside <- marx %msub% ok
+    x.inside <- as.psp(ends.inside, window=window, marks=marks.inside,
+                       check=FALSE)
+    if(!fragments)
+      return(x.inside)
+    #' now consider the rest
+    ends <- ends[!ok, , drop=FALSE]
+    in0 <- in0[!ok] 
+    in1 <- in1[!ok]
+    marx <- marx %msub% (!ok)
+    #' first clip segments to the range x \in [xmin, xmax]
+    #' use parametric coordinates
+    tx <- cbind(ifelse0NA(between(ends$x0, window$xrange)),
+                ifelse1NA(between(ends$x1, window$xrange)),
+                tvalue(ends$x0, ends$x1, window$xrange[1L]),
+                tvalue(ends$x0, ends$x1, window$xrange[2L]))
+    #' discard segments which do not lie in the x range 
+    nx <- apply(!is.na(tx), 1L, sum)
+    ok <- (nx >= 2)
+    if(!any(ok))
+      return(x.inside)
+    ends <- ends[ok, , drop=FALSE]
+    tx   <- tx[ok, , drop=FALSE]
+    in0  <- in0[ok]
+    in1  <- in1[ok]
+    marx <- marx %msub% ok
+    #' Clip the segments to the x range
+    tmin <- apply(tx, 1L, min, na.rm=TRUE)
+    tmax <- apply(tx, 1L, max, na.rm=TRUE)
+    dx <- ends$x1 - ends$x0
+    dy <- ends$y1 - ends$y0
+    ends.xclipped <- data.frame(x0=ends$x0 + tmin * dx,
+                                y0=ends$y0 + tmin * dy,
+                                x1=ends$x0 + tmax * dx,
+                                y1=ends$y0 + tmax * dy)
+    #' Now clip the segments to the range y \in [ymin, ymax]
+    ends <- ends.xclipped
+    in0 <- inside.owin(ends$x0, ends$y0, window)
+    in1 <- inside.owin(ends$x1, ends$y1, window)
+    ty <- cbind(ifelse0NA(in0),
+                ifelse1NA(in1),
+                tvalue(ends$y0, ends$y1, window$yrange[1L]),
+                tvalue(ends$y0, ends$y1, window$yrange[2L]))
+    #' discard segments which do not lie in the y range 
+    ny <- apply(!is.na(ty), 1L, sum)
+    ok <- (ny >= 2)
+    if(!any(ok))
+      return(x.inside)
+    ends <- ends[ok, , drop=FALSE]
+    ty   <- ty[ok, , drop=FALSE]
+    in0  <- in0[ok]
+    in1  <- in1[ok]
+    marx <- marx %msub% ok
+    #' Clip the segments to the y range
+    tmin <- apply(ty, 1L, min, na.rm=TRUE)
+    tmax <- apply(ty, 1L, max, na.rm=TRUE)
+    dx <- ends$x1 - ends$x0
+    dy <- ends$y1 - ends$y0
+    ends.clipped <- data.frame(x0=ends$x0 + tmin * dx,
+                               y0=ends$y0 + tmin * dy,
+                               x1=ends$x0 + tmax * dx,
+                               y1=ends$y0 + tmax * dy)
+    marks.clipped <- marx
+    #' OK - segments clipped
+    #' Put them together with the unclipped ones
+    ends.all <- rbind(ends.inside, ends.clipped)
+    marks.all <- marks.inside %mapp% marks.clipped
+    as.psp(ends.all, window=window, marks=marks.all, check=FALSE)
+  }
+
   small <- function(x) { abs(x) <= .Machine$double.eps }
+
   tvalue <- function(z0, z1, zt) {
     y1 <- z1 - z0
     yt <- zt - z0
@@ -75,65 +137,11 @@ cliprect.psp <- function(x, window, fragments=TRUE) {
     result <- ifelseXB(betwee, tval, NA)
     return(result)
   }
-  between <- function(x, r) { ((x-r[1L]) * (x-r[2L])) <= 0 }
-  tx <- cbind(ifelse0NA(between(ends$x0, window$xrange)),
-              ifelse1NA(between(ends$x1, window$xrange)),
-              tvalue(ends$x0, ends$x1, window$xrange[1L]),
-              tvalue(ends$x0, ends$x1, window$xrange[2L]))
-  # discard segments which do not lie in the x range 
-  nx <- apply(!is.na(tx), 1L, sum)
-  ok <- (nx >= 2)
-  if(!any(ok))
-    return(x.inside)
-  ends <- ends[ok, , drop=FALSE]
-  tx   <- tx[ok, , drop=FALSE]
-  in0  <- in0[ok]
-  in1  <- in1[ok]
-  marx <- marx %msub% ok
-  # Clip the segments to the x range
-  tmin <- apply(tx, 1L, min, na.rm=TRUE)
-  tmax <- apply(tx, 1L, max, na.rm=TRUE)
-  dx <- ends$x1 - ends$x0
-  dy <- ends$y1 - ends$y0
-  ends.xclipped <- data.frame(x0=ends$x0 + tmin * dx,
-                             y0=ends$y0 + tmin * dy,
-                             x1=ends$x0 + tmax * dx,
-                             y1=ends$y0 + tmax * dy)
-  # Now clip the segments to the range y \in [ymin, ymax]
-  ends <- ends.xclipped
-  in0 <- inside.owin(ends$x0, ends$y0, window)
-  in1 <- inside.owin(ends$x1, ends$y1, window)
-  ty <- cbind(ifelse0NA(in0),
-              ifelse1NA(in1),
-              tvalue(ends$y0, ends$y1, window$yrange[1L]),
-              tvalue(ends$y0, ends$y1, window$yrange[2L]))
-  # discard segments which do not lie in the y range 
-  ny <- apply(!is.na(ty), 1L, sum)
-  ok <- (ny >= 2)
-  if(!any(ok))
-    return(x.inside)
-  ends <- ends[ok, , drop=FALSE]
-  ty   <- ty[ok, , drop=FALSE]
-  in0  <- in0[ok]
-  in1  <- in1[ok]
-  marx <- marx %msub% ok
-  # Clip the segments to the y range
-  tmin <- apply(ty, 1L, min, na.rm=TRUE)
-  tmax <- apply(ty, 1L, max, na.rm=TRUE)
-  dx <- ends$x1 - ends$x0
-  dy <- ends$y1 - ends$y0
-  ends.clipped <- data.frame(x0=ends$x0 + tmin * dx,
-                             y0=ends$y0 + tmin * dy,
-                             x1=ends$x0 + tmax * dx,
-                             y1=ends$y0 + tmax * dy)
-  marks.clipped <- marx
-  # OK - segments clipped
-  # Put them together with the unclipped ones
-  ends.all <- rbind(ends.inside, ends.clipped)
-  marks.all <- marks.inside %mapp% marks.clipped
-  as.psp(ends.all, window=window, marks=marks.all, check=FALSE)
-}
 
+  between <- function(x, r) { ((x-r[1L]) * (x-r[2L])) <= 0 }
+
+  cliprect.psp
+})
 
 ############################
 #
@@ -155,8 +163,10 @@ clippoly.psp <- function(s, window, fragments=TRUE) {
   es <- s$ends
   x0s <- es$x0
   y0s <- es$y0
-  dxs <- es$x1 - es$x0
-  dys <- es$y1 - es$y0
+  x1s <- es$x1
+  y1s <- es$y1
+  dxs <- x1s - x0s
+  dys <- y1s - y0s
 
   bdry <- edges(window)
   nw <- bdry$n
@@ -202,36 +212,46 @@ clippoly.psp <- function(s, window, fragments=TRUE) {
   }
   # form all the chopped segments (whether in or out)
 
-  chopped <- s[numeric(0)]
-  chopped$window <- boundingbox(s$window, window)
+  #' initially empty
+  chopx0 <- chopy0 <- chopx1 <- chopy1 <- numeric(0)
+  chopmarks <- marx %msub% integer(0)
     
   for(seg in seq_len(ns)) {
-    segment <- s$ends[seg, , drop=FALSE]
+    #' coordinates of segment number 'seg'
+    segx0 <- x0s[seg]
+    segy0 <- y0s[seg]
+    segx1 <- x1s[seg]
+    segy1 <- y1s[seg]
+    if(has.marks)
+      segmarks <- marx %msub% seg
     if(!anyhit[seg]) {
-      # no intersection with boundary - add single segment
-      chopped$ends <- rbind(chopped$ends, segment)
-    if(has.marks) chopped$marks <- (chopped$marks) %mapp% (marx %msub% seg)
+      #' no intersection with boundary - add single segment
+      chopx0 <- c(chopx0, segx0)
+      chopy0 <- c(chopy0, segy0)
+      chopx1 <- c(chopx1, segx1)
+      chopy1 <- c(chopy1, segy1)
+      if(has.marks)
+        chopmarks <- chopmarks %mapp% segmarks
     } else {
-      # crosses boundary - add several pieces
+      #' crosses boundary - add several pieces
       tvals <- ts[seg,]
       tvals <- sort(tvals[hitting[seg,]])
-      x0 <- segment$x0
-      dx <- segment$x1 - x0
-      y0 <- segment$y0
-      dy <- segment$y1 - y0
-      newones <- data.frame(x0 = x0 + c(0,tvals) * dx,
-                            y0 = y0 + c(0,tvals) * dy,
-                            x1 = x0 + c(tvals,1) * dx,
-                            y1 = y0 + c(tvals,1) * dy)
-      chopped$ends <- rbind(chopped$ends, newones)
+      dx <- segx1 - segx0
+      dy <- segy1 - segy0
+      chopx0 <- c(chopx0, segx0 + c(0,tvals) * dx)
+      chopy0 <- c(chopy0, segy0 + c(0,tvals) * dy)
+      chopx1 <- c(chopx1, segx0 + c(tvals,1) * dx)
+      chopy1 <- c(chopy1, segy0 + c(tvals,1) * dy)
       if(has.marks) {
-        hitmarks <- marx %msub% seg
-        newmarks <- hitmarks %mrep% nrow(newones)
-        chopped$marks <-  (chopped$marks) %mapp% newmarks
+        npieces <- length(tvals) + 1L
+        chopmarks <- chopmarks %mapp% (segmarks %mrep% npieces)
       }
     }
   }
-  chopped$n <- nrow(chopped$ends)
+
+  chopped <- psp(chopx0, chopy0, chopx1, chopy1,
+                 window=boundingbox(Window(s), window),
+                 marks=chopmarks)
   
   # select those chopped segments which are inside the window
   mid <- midpoints.psp(chopped)
