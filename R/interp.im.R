8db4785223ce69453a6cf1f2ac7cc98c11305444
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2018-07-30 23:15:46 +0800

    Added option in interp.im

--- R/interp.im.R
+++ R/interp.im.R
@@ -1,7 +1,7 @@
 #
 # interp.im.R
 #
-#  $Revision: 1.4 $  $Date: 2017/02/07 07:47:20 $
+#  $Revision: 1.6 $  $Date: 2018/07/30 14:29:25 $
 #
 
 interp.im <- local({
@@ -14,7 +14,7 @@ interp.im <- local({
     result
   }
 
-  interp.im <- function(Z, x, y=NULL) {
+  interp.im <- function(Z, x, y=NULL, bilinear=FALSE) {
     stopifnot(is.im(Z))
     if(!is.null(levels(Z)))
       stop("Interpolation is undefined for factor-valued images")
@@ -22,6 +22,7 @@ interp.im <- local({
     x <- xy$x
     y <- xy$y
     ok <- inside.owin(x,y, as.owin(Z))
+    V <- Z$v
     ## get default lookup values (for boundary cases)
     fallback <- Z[ppp(x[ok], y[ok], window=as.rectangle(Z), check=FALSE)]
     ## Transform to grid coordinates
@@ -35,22 +36,31 @@ interp.im <- local({
     ylower <- floor(yy)
     cc <- as.integer(xlower) + 1L
     rr <- as.integer(ylower) + 1L
-    ## determine whether (x,y) is above or below antidiagonal in square
     dx <- xx - xlower
     dy <- yy - ylower
-    below <- (dx + dy <= 1)
-    ## if below, interpolate Z(x,y) = (1-x-y)Z(0,0) + xZ(1,0) + yZ(0,1)
-    ## if above, interpolate Z(x,y) = (x+y-1)Z(1,1) + (1-x)Z(0,1) + (1-y)Z(1,0)
-    V <- Z$v
-    values <- ifelse(below,
-                     ( (1-dx-dy)*lukimyu(cc,rr,V,fallback)
-                      + dx*lukimyu(cc+1,rr,V,fallback)
-                      + dy*lukimyu(cc,rr+1,V,fallback)
-                      ),
-                     ( (dx+dy-1)*lukimyu(cc+1,rr+1,V,fallback)
-                      + (1-dx)*lukimyu(cc,rr+1,V,fallback)
-                      + (1-dy)*lukimyu(cc+1,rr,V,fallback)
+    if(bilinear) {
+      ## 'orthodox'
+      values <- ((1-dx) * (1-dy) * lukimyu(cc,rr,V,fallback)
+                 + dx   * (1-dy) * lukimyu(cc+1,rr,V,fallback)
+                 + (1-dx) * dy   * lukimyu(cc,rr+1,V,fallback)
+                 + dx    * dy    * lukimyu(cc+1,rr+1,V,fallback)
+      )
+    } else {
+      ## original & default
+      ## determine whether (x,y) is above or below antidiagonal in square
+      below <- (dx + dy <= 1)
+      ## if below,interpolate Z(x,y) = (1-x-y)Z(0,0) + xZ(1,0) + yZ(0,1)
+      ## if above,interpolate Z(x,y) = (x+y-1)Z(1,1) + (1-x)Z(0,1) + (1-y)Z(1,0)
+      values <- ifelse(below,
+                      ( (1-dx-dy)*lukimyu(cc,rr,V,fallback)
+                       + dx*lukimyu(cc+1,rr,V,fallback)
+                       + dy*lukimyu(cc,rr+1,V,fallback)
+                       ),
+                      ( (dx+dy-1)*lukimyu(cc+1,rr+1,V,fallback)
+                       + (1-dx)*lukimyu(cc,rr+1,V,fallback)
+                       + (1-dy)*lukimyu(cc+1,rr,V,fallback)
                       ))
+    }
     result <- numeric(length(x))
     result[ok] <- values
     result[!ok] <- NA
