9a6f1ecccc22465d1ea00a50c738ac6dccdf8ea7
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2019-01-29 12:57:08 +0800

    Bug fix in beachcolours

--- R/colourschemes.R
+++ R/colourschemes.R
@@ -1,7 +1,7 @@
 #
 #  colourschemes.R
 #
-#  $Revision: 1.3 $  $Date: 2013/07/17 04:53:48 $
+#  $Revision: 1.4 $  $Date: 2019/01/29 04:32:44 $
 #
 
 beachcolourmap <- function(range, ...) {
@@ -13,26 +13,46 @@ beachcolourmap <- function(range, ...) {
 beachcolours <- function(range, sealevel = 0, monochrome=FALSE,
                          ncolours=if(monochrome) 16 else 64,
                          nbeach=1) {
+  check.range(range)
+  stopifnot(all(is.finite(range)))
+  check.1.real(sealevel)
+  check.in.range(sealevel, range)
+  check.1.integer(ncolours)
+  stopifnot(ncolours >= 3)
+  check.1.integer(nbeach)
+  stopifnot(nbeach >= 0)
+  stopifnot(nbeach <= ncolours + 2)
   if(monochrome)
     return(grey(seq(from=0,to=1,length.out=ncolours)))
-  stopifnot(is.numeric(range) && length(range) == 2)
-  stopifnot(all(is.finite(range)))
   depths <- range[1L]
   peaks <- range[2L]
   dv <- diff(range)/(ncolours - 1L)
   epsilon <- nbeach * dv/2
   lowtide <- max(sealevel - epsilon, depths)
   hightide <-  min(sealevel + epsilon, peaks)
-  countbetween <- function(a, b, delta) { max(0, round((b-a)/delta)) }
-  nsea <- countbetween(depths, lowtide, dv)
-  nbeach <- countbetween(lowtide,  hightide, dv)
-  nland <- countbetween(hightide,  peaks, dv)
+  nsea <- max(0L, floor((lowtide - depths)/dv))
+  nland <- max(0L, floor((peaks  - hightide)/dv))
+  discrep <- nsea + nland + nbeach - ncolours
+  if(discrep != 0) {
+    dd <- abs(discrep)
+    ss <- as.integer(-sign(discrep))
+    smallhalf <- dd/2L
+    largehalf <- dd - smallhalf
+    if(nsea < nland) {
+      nsea <- nsea + ss * smallhalf
+      nland <- nland + ss * largehalf
+    } else {
+      nland <- nland + ss * smallhalf
+      nsea <- nsea + ss * largehalf
+    }      
+    if(nsea + nland + nbeach != ncolours)
+      warning("Internal error: incorrect adjustment of length in beachcolours")
+  }
   colours <- character(0)
   if(nsea > 0)  colours <- rev(rainbow(nsea, start=3/6,end=4/6)) # cyan/blue
   if(nbeach > 0)  colours <- c(colours,
-                             rev(rainbow(nbeach, start=3/12,end=5/12))) # green
+                               rev(rainbow(nbeach, start=3/12,end=5/12))) # green
   if(nland > 0)  colours <- c(colours,
                               rev(rainbow(nland, start=0, end=1/6)))  # red/yellow
   return(colours)
 }
-
