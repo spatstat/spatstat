241e8f7e713e869be1cf9cfe9820e97b4230148c
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2019-07-29 16:56:33 +1000

    Cross-validation on a network

--- R/bw.ppl.R
+++ R/bw.ppl.R
@@ -1,12 +1,16 @@
-#
-#   bw.ppl.R
-#
-#   Likelihood cross-validation for kernel smoother of point pattern
-#
-#   $Revision: 1.7 $ $Date: 2017/01/28 06:30:21 $
-#
+#'
+#'   bw.ppl.R
+#'
+#'   Likelihood cross-validation for kernel smoother of point pattern
+#'
+#'   bw.ppl    class ppp
+#'   bw.lppl   class lpp
+#' 
+#'   $Revision: 1.9 $ $Date: 2019/07/29 05:52:09 $
+#'
 
-bw.ppl <- function(X, ..., srange=NULL, ns=16, sigma=NULL, weights=NULL) {
+bw.ppl <- function(X, ..., srange=NULL, ns=16, sigma=NULL,
+                   weights=NULL, shortcut=FALSE) {
   stopifnot(is.ppp(X))
   if(!is.null(sigma)) {
     stopifnot(is.numeric(sigma) && is.vector(sigma))
@@ -19,14 +23,65 @@ bw.ppl <- function(X, ..., srange=NULL, ns=16, sigma=NULL, weights=NULL) {
     sigma <- geomseq(from=srange[1L], to=srange[2L], length.out=ns)
   }
   cv <- numeric(ns)
-  for(i in 1:ns) {
-    si <- sigma[i]
-    lamx <- density(X, sigma=si, at="points", leaveoneout=TRUE, weights=weights)
-    lam <- density(X, sigma=si, weights=weights)
-    cv[i] <- sum(log(lamx)) - integral.im(lam)
+  if(shortcut) {
+    for(i in 1:ns) {
+      si <- sigma[i]
+      lamx <- density(X, sigma=si, at="points", leaveoneout=TRUE,
+                    weights=weights, ...)
+      cv[i] <- sum(log(lamx))
+    }
+  } else {
+    for(i in 1:ns) {
+      si <- sigma[i]
+      lamx <- density(X, sigma=si, at="points", leaveoneout=TRUE,
+                      weights=weights, ...)
+      lam <- density(X, sigma=si,
+                     weights=weights, ...)
+      cv[i] <- sum(log(lamx)) - integral.im(lam)
+    }
   }
   result <- bw.optim(cv, sigma, iopt=which.max(cv), 
                      creator="bw.ppl",
+                     criterion="Likelihood Cross-Validation",
+                     unitname=unitname(X))
+  return(result)
+}
+
+
+bw.lppl <- function(X, ..., srange=NULL, ns=16, sigma=NULL,
+                   weights=NULL, distance="euclidean", shortcut=FALSE) {
+  stopifnot(is.lpp(X))
+  if(!is.null(sigma)) {
+    stopifnot(is.numeric(sigma) && is.vector(sigma))
+    ns <- length(sigma)
+  } else {
+    if(!is.null(srange)) check.range(srange) else {
+      dd <- diameter(Frame(X))                                               
+      ss <- bw.scott.iso(X)
+      srange <- range(c(ss/10, ss*5, dd/5))
+    }
+    sigma <- geomseq(from=srange[1L], to=srange[2L], length.out=ns)
+  }
+  cv <- numeric(ns)
+  if(shortcut) {
+    for(i in 1:ns) {
+      si <- sigma[i]
+      lamx <- density(X, sigma=si, at="points", leaveoneout=TRUE,
+                      weights=weights, distance=distance, ...)
+      cv[i] <- sum(log(lamx))
+    }
+  } else {
+    for(i in 1:ns) {
+      si <- sigma[i]
+      lamx <- density(X, sigma=si, at="points", leaveoneout=TRUE,
+                      weights=weights, distance=distance, ...)
+      lam <- density(X, sigma=si,
+                     weights=weights, distance=distance, ...)
+      cv[i] <- sum(log(lamx)) - integral.im(lam)
+    }
+  }
+  result <- bw.optim(cv, sigma, iopt=which.max(cv), 
+                     creator="bw.lppl",
                      criterion="Likelihood Cross-Validation",
                      unitname=unitname(X))
   return(result)
