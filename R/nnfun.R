72ad86cde7cd0db11edf8f117e6805e5f3100722
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2019-09-16 18:52:40 +0800

    nnfun can return mark value

--- R/nnfun.R
+++ R/nnfun.R
@@ -3,36 +3,64 @@
 #
 #   nearest neighbour function (returns a function of x,y)
 #
-#   $Revision: 1.7 $   $Date: 2018/04/09 07:43:26 $
+#   $Revision: 1.8 $   $Date: 2019/09/16 10:14:28 $
 #
 
 nnfun <- function(X, ...) {
   UseMethod("nnfun")
 }
 
-nnfun.ppp <- function(X, ..., k=1) {
+nnfun.ppp <- function(X, ..., k=1, value=c("index", "mark")) {
   # this line forces X to be bound
   stopifnot(is.ppp(X))
   if(length(k) != 1) stop("k should be a single integer")
-  g <- function(x,y=NULL) {
-    Y <- xy.coords(x, y)[c("x", "y")]
-    nncross(Y, X, what="which", k=k)
-  }
-  attr(g, "Xclass") <- "ppp"
-  g <- funxy(g, as.rectangle(as.owin(X)))
+  value <- match.arg(value)
+  switch(value,
+         index = {
+           gi <- function(x,y=NULL) {
+             Y <- xy.coords(x, y)[c("x", "y")]
+             nncross(Y, X, what="which", k=k)
+           }
+           attr(gi, "Xclass") <- "ppp"
+           g <- funxy(gi, as.rectangle(as.owin(X)))
+         },
+         mark = {
+           stopifnot(is.marked(X))
+           marx <- as.data.frame(marks(X))[,1]
+           gm <- function(x,y=NULL) {
+             Y <- xy.coords(x, y)[c("x", "y")]
+             marx[nncross(Y, X, what="which", k=k)]
+           }
+           attr(gm, "Xclass") <- "ppp"
+           g <- funxy(gm, as.rectangle(as.owin(X)))
+         })
   class(g) <- c("nnfun", class(g))
   return(g)
 }
 
-nnfun.psp <- function(X, ...) {
+nnfun.psp <- function(X, ..., value=c("index", "mark")) {
   # this line forces X to be bound
   stopifnot(is.psp(X))
-  g <- function(x,y=NULL) {
-    Y <-  xy.coords(x, y)[c("x", "y")]
-    nncross(Y, X, what="which")
-  }
-  attr(g, "Xclass") <- "psp"
-  g <- funxy(g, as.rectangle(as.owin(X)))
+  value <- match.arg(value)
+  switch(value,
+         index = {
+           gi <- function(x,y=NULL) {
+             Y <-  xy.coords(x, y)[c("x", "y")]
+             nncross(Y, X, what="which")
+           }
+           attr(gi, "Xclass") <- "psp"
+           g <- funxy(gi, as.rectangle(as.owin(X)))
+         },
+         mark = {
+           stopifnot(is.marked(X))
+           marx <- as.data.frame(marks(X))[,1]
+           gm <- function(x,y=NULL) {
+             Y <-  xy.coords(x, y)[c("x", "y")]
+             marx[nncross(Y, X, what="which")]
+           }
+           attr(gm, "Xclass") <- "psp"
+           g <- funxy(gm, as.rectangle(as.owin(X)))
+         })
   class(g) <- c("nnfun", class(g))
   return(g)
 }
@@ -50,8 +78,13 @@ as.im.nnfun <- function(X, W=NULL, ...,
   if(approx && is.null(W)) {
     env <- environment(X)
     Xdata  <- get("X", envir=env)
-    k <- mget("k", envir=env, inherits=FALSE, ifnotfound=list(1))[[1]]
+    k <- mget("k", envir=env, inherits=FALSE, ifnotfound=list(1))[[1L]]
+    value <- mget("value", envir=env, ifnotfound=list(NULL))[[1L]] 
     Z <- nnmap(Xdata, k=k, what="which", eps=eps, dimyx=dimyx, xy=xy)
+    if(identical(value, "mark")) {
+      marx <- get("marx", envir=env)
+      Z <- eval.im(marx[Z])
+    }
     if(!is.null(na.replace))
       Z$v[is.null(Z$v)] <- na.replace
     return(Z)
@@ -66,15 +99,19 @@ as.im.nnfun <- function(X, W=NULL, ...,
 print.nnfun <- function(x, ...) {
   env <- environment(x)
   X <- get("X", envir=env)
-  k <- mget("k", envir=env, inherits=FALSE, ifnotfound=list(1))[[1]]
+  k <- mget("k", envir=env, inherits=FALSE, ifnotfound=list(1))[[1L]]
+  v <- mget("value", envir=env, ifnotfound=list(NULL))[[1L]]
   xtype <- attr(x, "Xclass")
   typestring <- switch(xtype,
                        ppp="point pattern",
                        psp="line segment pattern",
                        paste("object of class", sQuote(xtype)))
   Kth <- if(k == 1) "Nearest" else paste0(ordinal(k), "-Nearest")
-  cat(paste(Kth, "Neighbour Index function for ", typestring, "\n"))
+  cat(paste(Kth, "Neighbour",
+            if(is.null(v)) "Index" else "Mark",
+            "function for ", typestring, "\n"))
   print(X)
+
   return(invisible(NULL))
 }
 
--- R/nnfunlpp.R
+++ R/nnfunlpp.R
@@ -3,23 +3,37 @@
 #
 #   method for 'nnfun' for class 'lpp'
 #
-#   $Revision: 1.2 $ $Date: 2016/08/21 04:33:47 $
+#   $Revision: 1.3 $ $Date: 2019/09/16 10:14:18 $
 #
 
 nnfun.lpp <- local({
 
-  nnfun.lpp <- function(X, ..., k=1) {
+  nnfun.lpp <- function(X, ..., k=1, value=c("index", "mark")) {
     stopifnot(inherits(X, "lpp"))
     force(X)
     force(k)
+    value <- match.arg(value)
     L <- as.linnet(X)
-    f <- function(x, y=NULL, seg=NULL, tp=NULL, ...) {
-      # L is part of the environment
-      Y <- as.lpp(x=x, y=y, seg=seg, tp=tp, L=L)
-      i <- nncross.lpp(Y, X, what="which", k=k)
-      return(i)
-    }
-    f <- linfun(f, L)
+    switch(value,
+           index = {
+             fi <- function(x, y=NULL, seg=NULL, tp=NULL, ...) {
+               ## L is part of the environment
+               Y <- as.lpp(x=x, y=y, seg=seg, tp=tp, L=L)
+               i <- nncross.lpp(Y, X, what="which", k=k)
+               return(i)
+             }
+             f <- linfun(fi, L)
+           },
+           mark = {
+             stopifnot(is.marked(X))
+             marx <- as.data.frame(marks(X))[,1]
+             fm <- function(x, y=NULL, seg=NULL, tp=NULL, ...) {
+               Y <- as.lpp(x=x, y=y, seg=seg, tp=tp, L=L)
+               i <- nncross.lpp(Y, X, what="which", k=k)
+               return(marx[i])
+             }
+             f <- linfun(fm, L)
+           })
     attr(f, "explain") <- uitleggen
     return(f)
   }
@@ -35,6 +49,10 @@ nnfun.lpp <- local({
       cat(paste("k =", commasep(k), "\n"))
     }
     print(X)
+    v <- mget("value", envir=env, ifnotfound=list(NULL))[[1L]]
+    splat("Function returns the",
+          if(identical(v, "mark")) "mark value" else "index",
+          "of the neighbour")
   }
 
   nnfun.lpp
