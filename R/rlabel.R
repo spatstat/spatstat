4dd8e855f55a9381f68526090cd00e6abe2b97f6
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2020-10-23 23:36:58 +0800

    Extension to rlabel

--- R/rlabel.R
+++ R/rlabel.R
@@ -3,31 +3,49 @@
 #
 #   random (re)labelling
 #
-#   $Revision: 1.12 $   $Date: 2019/04/25 02:59:12 $
+#   $Revision: 1.13 $   $Date: 2020/10/23 15:18:00 $
 #
 #
-rlabel <- function(X, labels=marks(X), permute=TRUE, nsim=1, drop=TRUE) {
-  stopifnot(is.ppp(X) || is.lpp(X) || is.pp3(X) || is.ppx(X) || is.psp(X))
-  if(is.null(labels))
-    stop("labels not given and marks not present")
-  nthings <- nobjects(X)
-  things <- if(is.psp(X)) "segments" else "points"
-  if(is.vector(labels) || is.factor(labels)) {
-    nlabels <- length(labels)
-    if(permute && (nlabels != nthings))
-      stop(paste("length of labels vector does not match number of", things))
-    Y <- replicate(nsim,
-                   X %mark% sample(labels, nthings, replace=!permute),
-                   simplify=FALSE)
-  } else if(is.data.frame(labels) || is.hyperframe(labels)) {
-    nlabels <- nrow(labels)
-    if(permute && (nlabels != nthings))
-      stop(paste("number of rows of data frame does not match number of",
-                 things))
-    Y <- replicate(nsim,
-                   X %mark% labels[sample(1:nlabels, nthings, replace=!permute), ,drop=FALSE],
-                   simplify=FALSE)
-  } else stop("Format of labels argument is not understood")
-  return(simulationresult(Y, nsim, drop))
-}
+rlabel <- local({
 
+  resample <- function(x, replace=FALSE) {
+    x[sample(length(x), replace=replace)]
+  }
+
+  rlabel <- function(X, labels=marks(X), permute=TRUE,
+                     group=NULL, ..., nsim=1, drop=TRUE) {
+    stopifnot(is.ppp(X) || is.lpp(X) || is.pp3(X) || is.ppx(X) || is.psp(X))
+    if(is.null(labels))
+      stop("labels not given and marks not present")
+    singlecolumn <- (length(dim(labels)) < 2)
+    nthings <- nobjects(X)
+    things <- if(is.psp(X)) "segments" else "points"
+    nlabels <- if(singlecolumn) length(labels) else nrow(labels)
+    if((nlabels != nthings) && (permute || !is.null(group))) 
+      stop(paste(if(singlecolumn) "Length" else "Number of rows",
+                 "of labels does not match the number of",
+                 things),
+           call.=FALSE)
+    ##
+    if(is.null(group)) {
+      Y <- replicate(nsim, {
+        X %mark% marksubset(labels, sample(nlabels, nthings, replace=!permute))
+      },
+      simplify=FALSE)
+    } else {
+      group <- marks(cut(X, group, ...))
+      seqn <- seq_len(nlabels)
+      pieces <- split(seqn, group)
+      Y <- replicate(nsim, {
+        X %mark% marksubset(labels,
+                            unsplit(lapply(pieces, resample, replace=!permute),
+                                    group))
+      },
+      simplify=FALSE)
+    }
+    ## 
+    return(simulationresult(Y, nsim, drop))
+  }
+
+  rlabel
+})
