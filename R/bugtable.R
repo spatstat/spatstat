a636de547389b01de1d19b8d0257ff3c59bf0946
Adrian Baddeley <adrian.baddeley@curtin.edu.au>
2021-09-06 13:55:41 +0800

    Redesigned interface for news, bugfixes

--- R/bugtable.R
+++ R/bugtable.R
@@ -1,74 +1,85 @@
 #'
 #'        bugtable.R
 #' 
-#'    $Revision: 1.6 $ $Date: 2020/02/21 08:16:43 $
+#'    $Revision: 1.10 $ $Date: 2021/09/06 05:28:59 $
 
 bugfixes <- function(sinceversion=NULL, sincedate=NULL,
-                     package="spatstat",
+                     package=spatstat.family(),
                      show=TRUE) {
-  if(package == "spatstat" && ("book" %in% list(sinceversion, sincedate))) {
+  package <- as.character(package)
+  is.spat <- package %in% spatstat.family(TRUE, TRUE)
+  if("book" %in% list(sinceversion, sincedate)) {
     ## special usage
-    sinceversion <- "1.42-1"
-    sincedate <- NULL
+    sinceversion <- NULL
+    sincedate    <- "2015-06-05"
   }
-  if("all" %in% list(sinceversion, sincedate)) {
-    ## no date constraint
-    a <- eval(substitute(news(grepl("^BUG", Category),
-                              package=package)))
-  } else if(!is.null(sincedate) && package != "spatstat") {
-    #' another package: news items after specified date
-    ne <- news(package=package)
-    if(is.null(ne) || is.null(ne$Date) || anyNA(ne$Date))
-      stop(paste(if(is.null(ne)) "News" else "Date",
-                    "information is not available for package",
-                 sQuote(package)), call.=FALSE)
-    a <- eval(substitute(news(Date >= SD & grepl("^BUG", Category),
-                              package=package),
-                         list(SD=sincedate)))
-  } else {
-    #' determine a corresponding version number
-    if(is.null(sinceversion) && is.null(sincedate)) {
-      #' default is latest version
-      dfile <- system.file("DESCRIPTION", package=package)
-      sinceversion <- read.dcf(file=dfile, fields="Version")
-    } else if(!is.null(sincedate) && package == "spatstat") {
-      #' read spatstat release history table
-      fname <- system.file("doc", "packagesizes.txt", package="spatstat")
-      p <- read.table(fname, header=TRUE, stringsAsFactors=FALSE)
-      #' find earliest package version on or after the given date
-      imin <- with(p, min(which(as.Date(date) >= sincedate)))
-      sinceversion <- p[imin, "version"]
+  n <- length(package)
+  z <- NULL
+  for(i in seq_len(n)) {
+    packi <- package[i]
+    if("all" %in% list(sinceversion, sincedate)) {
+      ## no date constraint - show all news
+      a <- eval(substitute(news(grepl("^BUG", Category),
+                                package=packi)))
+    } else if(!is.null(sincedate) && !is.spat[i]) {
+      #' news items after specified date
+      #' non-spatstat package
+      ne <- news(package=packi)
+      if(is.null(ne) || is.null(ne$Date) || anyNA(ne$Date))
+        stop(paste(if(is.null(ne)) "News" else "Date",
+                   "information is not available for package",
+                   sQuote(packi)), call.=FALSE)
+      a <- eval(substitute(news(Date >= SD & grepl("^BUG", Category),
+                                package=packi),
+                           list(SD=sincedate)))
+    } else {
+      #' determine a corresponding version number
+      if(is.null(sinceversion) && is.null(sincedate)) {
+        #' default is latest version
+        dfile <- system.file("DESCRIPTION", package=packi)
+        sinceversion <- read.dcf(file=dfile, fields="Version")
+      } else if(!is.null(sincedate) && is.spat[i]) {
+        #' read release history table
+        fname <- system.file("doc", "packagesizes.txt", package=packi)
+        p <- read.table(fname, header=TRUE, stringsAsFactors=FALSE)
+        #' find earliest package version on or after the given date
+        imin <- with(p, min(which(as.Date(date) >= sincedate)))
+        sinceversion <- p[imin, "version"]
+      }
+      a <- eval(substitute(news(Version >= sv & grepl("^BUG", Category),
+                                package=packi),
+                           list(sv=sinceversion)))
     }
-    a <- eval(substitute(news(Version >= sv & grepl("^BUG", Category),
-                              package=package),
-                         list(sv=sinceversion)))
+    #' convert format
+    if(is.data.frame(a) && nrow(a) > 0) {
+      #' split each entry into lines
+      alines <- strsplit(a$Text, "\n")
+      #' extract first line
+      f <- unname(sapply(alines, "[", i=1L))
+      #' extract body
+      b <- unname(lapply(alines, "[", i=-1L))
+      b <- unname(sapply(b, paste, collapse="\n"))
+      #' extract header from first line
+      h <- unname(sapply(strsplit(f, ":"), "[", i=1L))
+      h <- unname(sapply(strsplit(h, ","), "[", i=1L))
+      h <- unname(sapply(strsplit(h, " "), "[", i=1L))
+      #' rebuild
+      zi <- data.frame(Header=h,
+                       Firstline=f,
+                       Body=b,
+                       Version=a$Version,
+                       stringsAsFactors=FALSE)
+      if(n > 1) zi$Package <- packi
+      z <- if(i == 1) zi else rbind(z, zi)
+    } 
   }
-  if(!is.data.frame(a) || nrow(a) == 0) {
-    if(show) message("No bugs reported")
-    return(invisible(NULL))
-  }
-  #' split each entry into lines
-  alines <- strsplit(a$Text, "\n")
-  #' extract first line
-  f <- unname(sapply(alines, "[", i=1L))
-  #' extract body
-  b <- unname(lapply(alines, "[", i=-1L))
-  b <- unname(sapply(b, paste, collapse="\n"))
-  #' extract header from first line
-  h <- unname(sapply(strsplit(f, ":"), "[", i=1L))
-  h <- unname(sapply(strsplit(h, ","), "[", i=1L))
-  h <- unname(sapply(strsplit(h, " "), "[", i=1L))
+  if(is.null(z)) return(NULL)
   #' sort by header
-  oo <- order(h, f)
-  #' rebuild
-  z <- data.frame(Header=h[oo],
-                  Firstline=f[oo],
-                  Body=b[oo],
-                  Version=a$Version[oo],
-                  stringsAsFactors=FALSE)
+  oo <- with(z, order(Header, Firstline))
+  z <- z[oo, ]
+  #' wrap up
   class(z) <- c("bugtable", class(z))
-  if(!show) return(z)
-  page(z, method="print")
+  if(show) page(z, method="print")
   return(invisible(z))
 }
 
@@ -76,15 +87,19 @@ class(bugfixes) <- "autoexec"
               
 print.bugtable <- function(x, ...) {
   hprev <- ""
+  haspack <- "Package" %in% colnames(x)
   for(i in seq_len(nrow(x))) {
     h <- x$Header[i]
     f <- x$Firstline[i]
+    v <- x$Version[i]
+    p <- if(haspack) x$Package[i] else NULL
+    b <- x$Body[i]
     if(h != hprev) {
       # new main header
       cat("\n***", h, "***\n", fill=TRUE)
     }
-    cat(x$Version[i], ":", f, fill=TRUE)
-    cat(x$Body[i], "\n", fill=TRUE)
+    if(haspack) cat(p, v, ":", f, fill=TRUE) else cat(v, ":", f, fill=TRUE)
+    cat(b, "\n", fill=TRUE)
     hprev <- h
   }
   return(invisible(NULL))
--- R/news.R
+++ R/news.R
@@ -3,20 +3,42 @@
 #
 #  News and warnings
 #
-latest.news <- function(package="spatstat", doBrowse=FALSE, major=TRUE) {
-  ## get version number
-  v <- read.dcf(file=system.file("DESCRIPTION", package=package),
-                fields="Version")
-  if(major) {
-    ## the current major version
-    vp <- package_version(v)
-    vv <- unlist(vp)
-    v <- paste0(vv[1], ".", vv[2])
+latest.news <- function(package=spatstat.family(), doBrowse=FALSE, major=TRUE) {
+  stopifnot(is.character(package))
+  n <- length(package)
+  result <- vector(mode="list", length=n)
+  names(result) <- package
+  for(i in seq_len(n)) {
+    pack <- package[i]
+    ## get version number
+    v <- read.dcf(file=system.file("DESCRIPTION", package=pack),
+                  fields="Version")
+    if(major) {
+      ## the current major version
+      vp <- package_version(v)
+      vv <- unlist(vp)
+      v <- paste0(vv[1], ".", vv[2])
+    }
+    ne <- eval(substitute(news(Version >= v0, package=pack), list(v0=v)))
+    if(n > 1 && !doBrowse) {
+      hdr <- paste0("Package ", sQuote(pack), ":")
+      if(interactive()) readline(hdr) else cat(paste(hdr, "\n"))
+    }
+    page(ne, method="print", doBrowse=doBrowse)
+    result[[i]] <- ne
   }
-  ne <- eval(substitute(news(Version >= v0, package=package), list(v0=v)))
-  page(ne, method="print", doBrowse=doBrowse)
-  return(invisible(ne))
+  if(n == 1) result <- result[[1]]
+  return(invisible(result))
 }
 
 class(latest.news) <- "autoexec"
 
+spatstat.family <- function(subpackages=TRUE, extensions=FALSE) {
+  sub <- c("spatstat.utils", "spatstat.data", "spatstat.sparse",
+           "spatstat.geom", "spatstat.core", "spatstat.linnet",
+           "spatstat")
+  ext <- c("spatstat.gui", "spatstat.local", "spatstat.Knet")
+  result <- c(if(subpackages) sub else NULL,
+              if(extensions) ext else NULL)
+  as.character(result)
+}
